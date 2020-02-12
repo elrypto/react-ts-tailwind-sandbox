c3c6c18b175c3e56d728eae44365cde45a45825e
lrypto <lloydrebello@gmail.com>
2020-02-11 19:54:50 -0500

    overlapping cards

--- src/components/ui/PropertyCard.tsx
+++ src/components/ui/PropertyCard.tsx
@@ -17,24 +17,27 @@ export const PropertyCard =
 
 
     return (
-      <div className="bg-white border rounded-lg overflow-hidden">
-        <div className="relative bg-red-500 pb-2/3">
-          <img className="absolute h-full w-full h-48 w-full object-cover" src={imageUrl} alt={imageAlt} />
+      <div>
+        <div className="relative pb-5/6 rounded-lg">
+          <img className="absolute h-full w-full h-48 w-full object-cover rounded-lg shadow-md" src={imageUrl} alt={imageAlt} />
         </div>
-        <div className="p-6">
-          <div className="flex items-baseline">
-            <Badge>New</Badge>
-            <div className="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
-              {beds} beds &bull; {baths} baths
+        <div className="relative px-4 -mt-16">
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <div className="flex items-baseline">
+              <Badge>New</Badge>
+              <div className="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
+                {beds} beds &bull; {baths} baths
           </div>
+            </div>
+            <h4 className="mt-1 font-semibold text-lg leading-tight truncate">{title}</h4>
+            <div className="mt-1">
+              {formattedPrice}
+              <span className="text-gray-600 text-sm">/ wk</span>
+            </div>
+            <Stars rating={rating} numberOfReviews={reviewCount} />
           </div>
-          <h4 className="mt-1 font-semibold text-lg leading-tight truncate">{title}</h4>
-          <div className="mt-1">
-            {formattedPrice}
-            <span className="text-gray-600 text-sm">/ wk</span>
-          </div>
-          <Stars rating={rating} numberOfReviews={reviewCount} />
         </div>
+
       </div>
     )
   }
