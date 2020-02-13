565eaaa616eb636b60e0f59d56b5f2879a932cc4
lrypto <lloydrebello@gmail.com>
2020-02-13 15:15:47 -0500

    added avatar with dropdown skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BuildNavBar } from './pages/BuildNavBar';
 import { UpAndRunning } from './pages/UpAndRunning';
+import { DropDown } from './pages/DropDown';
 
 const App = () => <BuildNavBar />
 
--- src/pages/BuildNavBar.tsx
+++ src/pages/BuildNavBar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import InvertedLogo from './../img/logo-inverted.svg';
+import { AccountDropDown } from '../components/common/AccountDropDown';
 
 export const BuildNavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,7 +19,6 @@ export const BuildNavBar = () => {
             <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
               {isOpen ?
                 <path d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z" />
-
                 :
                 <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
               }
@@ -27,13 +27,13 @@ export const BuildNavBar = () => {
         </div>
       </div>
       {
-        <div className={isOpen ? 'block px-2 pt-2 pb-4 sm:block' : 'hidden sm:flex sm:p-0'}>
+        <nav className={isOpen ? 'block px-2 pt-2 pb-4 sm:block' : 'hidden sm:flex sm:p-0'}>
           <a className="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800" href="#">List your property</a>
           <a className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2" href="#">Trips</a>
           <a className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2" href="#">Messages</a>
-        </div>
+          <AccountDropDown twClasses="ml-6" />
+        </nav>
       }
-
     </header>
   )
 }
