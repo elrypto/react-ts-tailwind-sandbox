1d9f8695001c1fab88f3481ba17917caa8a844c0
lrypto <lloydrebello@gmail.com>
2020-02-15 13:22:54 -0500

    Revert "fixed test configuration"
    This reverts commit 111a9b25fafc90c58e9a0fd9a4fe2693654a7b56.

---
+++ src/pages/DropDown.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AccountDropDown } from '../components/common/AccountDropDown'
+
+export const DropDown = () => {
+  return (
+    <>
+      <AccountDropDown />
+    </>
+  )
+}
+
